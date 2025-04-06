d4ce81d90fda415e362dab538be6657f00d6ab24
ksidharth8 <btech10180.23@bitmesra.ac.in>
2025-04-07 00:05:39 +0530

    Add main React application setup

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Editor from './pages/Editor';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import AuthCallback from './pages/AuthCallback';
+import './App.css';
+
+function App() {
+  return (
+    <AuthProvider>
+      <ThemeProvider>
+        <Router>
+          <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <Navbar />
+            <main className="container mx-auto px-4 py-8">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/editor/:roomId" element={<Editor />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/auth/callback" element={<AuthCallback />} />
+              </Routes>
+            </main>
+          </div>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
+  );
+}
+
+export default App; 
